c078705441fc6cf4099087d9af1572f896763583
winningMove <dariopapic+github@gmail.com>
2024-09-01 14:33:25 +0200

    fix: replaced dns lookup with manual url verification

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ const dns = require("node:dns");
 const shortToFullUrlMap = {};
 let shortUrlAddress = 1;
 
+app.use(function setAccessHeader(req, res, next) {
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  next();
+});
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use("/public", express.static(`${__dirname}/public`));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
   res.sendFile(`${__dirname}/views/index.html`);
@@ -20,12 +24,33 @@ app.post("/api/shorturl", function (req, res) {
   const url = req.body.url;
   if (!URL.canParse(url)) {
     return res.json({ error: "invalid url" });
+  } else {
+    const urlObj = new URL(url);
+    if (!/^https?:$/.test(urlObj.protocol)) {
+      return res.json({ error: "invalid url" });
+    }
+  }
+  let shortUrl;
+  if (Object.values(shortToFullUrlMap).includes(url)) {
+    shortUrl = Object.keys(shortToFullUrlMap).find(
+      (k) => shortToFullUrlMap[k] === url
+    );
+  } else {
+    shortUrl = `${shortUrlAddress++}`;
+    shortToFullUrlMap[shortUrl] = url;
   }
 
+  res.json({
+    original_url: url,
+    short_url: shortUrl,
+  });
+
+  /* check if changing dns lookup to manual URL parsing makes the tests work
   const urlObj = new URL(url);
   dns.lookup(urlObj.host, (err) => {
     if (err) return res.json({ error: "invalid url" });
 
+    console.log("This code is being touched");
     let shortUrl;
     if (Object.values(shortToFullUrlMap).includes(url)) {
       shortUrl = Object.keys(shortToFullUrlMap).find(
@@ -42,6 +67,7 @@ app.post("/api/shorturl", function (req, res) {
       short_url: shortUrl,
     });
   });
+  */
 });
 
 app.get("/api/shorturl/:shorturl", function (req, res) {
